928d0c670fce80c4c65fecf518b1e59142ed2467
Das <adas162534@gmail.com>
2017-10-22 01:28:46 +0530

    Added SimpleFragmentPagerAdapter class

---
+++ app/src/main/java/com/example/yashladha/android_seller/classes/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,48 @@
+package com.example.yashladha.android_seller.classes;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+/**
+ * Created by dell pc on 16-10-2017.
+ */
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    private Context mContext;
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm, Context context) {
+
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new DisplayFrag();
+        } else if(position == 1){
+            return new SalesFrag();
+        } else {
+            return new OrdersFrag();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return "Products";
+        } else if(position == 1){
+            return "Sales Analysis";
+        } else{
+            return "Orders";
+        }
+    }
+}
