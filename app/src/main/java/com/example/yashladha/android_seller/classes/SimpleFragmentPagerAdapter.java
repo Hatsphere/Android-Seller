bb7522cbd54f6e24033f4bf508c409c148ebfe59
Das <adas162534@gmail.com>
2017-10-22 03:00:41 +0530

    The three fragments on the hom page created(front-end)

--- app/src/main/java/com/example/yashladha/android_seller/classes/SimpleFragmentPagerAdapter.java
+++ app/src/main/java/com/example/yashladha/android_seller/classes/SimpleFragmentPagerAdapter.java
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.example.yashladha.android_seller.fragments.DisplayFrag;
+import com.example.yashladha.android_seller.fragments.OrdersFrag;
+import com.example.yashladha.android_seller.fragments.SalesFrag;
+
 
 /**
  * Created by dell pc on 16-10-2017.
