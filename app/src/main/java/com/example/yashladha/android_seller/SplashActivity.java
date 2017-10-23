f95b1aa2fe339e0063d69213c4f178a53e3febf0
Shivvarya <shivvratvarya@gmail.com>
2017-10-23 09:58:25 +0530

    Added Splash File

--- app/src/main/java/com/example/yashladha/android_seller/SplashActivity.java
+++ app/src/main/java/com/example/yashladha/android_seller/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.yashladha.android_seller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -14,7 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        }, 5000);
 
 
     }
