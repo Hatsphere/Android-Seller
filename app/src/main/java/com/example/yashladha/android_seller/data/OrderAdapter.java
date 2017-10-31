494925ac4df65c49eb3abf7d941e0b250b826e73
Das <adas162534@gmail.com>
2017-10-31 21:47:16 +0530

    Made changes to the Display and Order Fragments

--- app/src/main/java/com/example/yashladha/android_seller/data/OrderAdapter.java
+++ app/src/main/java/com/example/yashladha/android_seller/data/OrderAdapter.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
