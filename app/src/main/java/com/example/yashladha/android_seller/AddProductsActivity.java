3cceaec029537a6546f2551e0cfac8430a3bfd52
Shivvarya <shivvratvarya@gmail.com>
2017-11-03 09:46:17 +0530

    changed add_product activity

--- app/src/main/java/com/example/yashladha/android_seller/AddProductsActivity.java
+++ app/src/main/java/com/example/yashladha/android_seller/AddProductsActivity.java
@@ -4,23 +4,25 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.example.yashladha.android_seller.fragments.DisplayFrag;
 
 public class AddProductsActivity extends AppCompatActivity {
 
     ImageView ivAdd;
-    TextView tvAddPhoto,tvProductName,tvProDes,tvNewPrice,tvOriginalPrice,tvDiscount,tvExchange;
-    EditText etProductName,etProDes,etNewPrice,etOriginalPrice,etDiscount,etExchange;
+    TextView tvAddPhoto, tvProductName, tvProDes,  tvOriginalPrice, tvDiscount, tvCategory;
+    EditText etProductName, etProDes, etOriginalPrice, etDiscount, etCategory;
     Button btDone;
+    ToggleButton tbOnSale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,27 +31,27 @@ public class AddProductsActivity extends AppCompatActivity {
 
         ivAdd = (ImageView) findViewById(R.id.ivAdd);
 
-        tvAddPhoto = (TextView)findViewById(R.id.tvAddPhoto);
-        tvProductName = (TextView)findViewById(R.id.tvProductName);
-        tvProDes = (TextView)findViewById(R.id.tvProDes);
-        tvNewPrice = (TextView)findViewById(R.id.tvNewPrice);
-        tvOriginalPrice = (TextView)findViewById(R.id.tvOriginalPrice);
-        tvDiscount = (TextView)findViewById(R.id.tvDiscount);
-        tvExchange = (TextView)findViewById(R.id.tvExchange);
+        tvAddPhoto = (TextView) findViewById(R.id.tvAddPhoto);
+        tvProductName = (TextView) findViewById(R.id.tvProductName);
+        tvProDes = (TextView) findViewById(R.id.tvProDes);
+        tvOriginalPrice = (TextView) findViewById(R.id.tvOriginalPrice);
+        tvDiscount = (TextView) findViewById(R.id.tvDiscount);
+        tvCategory = (TextView) findViewById(R.id.tvCategory);
 
-        etProductName = (EditText)findViewById(R.id.etProductName);
-        etProDes = (EditText)findViewById(R.id.etProDes);
-        etNewPrice = (EditText)findViewById(R.id.etNewPrice);
-        etOriginalPrice = (EditText)findViewById(R.id.etOriginalPrice);
-        etDiscount = (EditText)findViewById(R.id.etDiscount);
-        etExchange = (EditText)findViewById(R.id.etExchange);
+        etProductName = (EditText) findViewById(R.id.etProductName);
+        etProDes = (EditText) findViewById(R.id.etProDes);
+        etOriginalPrice = (EditText) findViewById(R.id.etOriginalPrice);
+        etDiscount = (EditText) findViewById(R.id.etDiscount);
+        etCategory = (EditText) findViewById(R.id.etCategory);
+
+        tbOnSale = (ToggleButton) findViewById(R.id.tbOnSale);
 
 
         ivAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(i,0);
+                Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, 0);
             }
         });
 
@@ -67,8 +69,8 @@ public class AddProductsActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 0 && resultCode == RESULT_OK && null != data) {
             Uri selectedImage = data.getData();
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
-            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
             cursor.moveToFirst();
             int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
             String picturePath = cursor.getString(columnIndex);
